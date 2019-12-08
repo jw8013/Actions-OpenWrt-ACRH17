f632d215cc3f20919911774dcad99e587edaa7c9
jw8013 <jw8013@qq.com>
2019-12-08 20:54:10 +0800

    no message

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/Ameykyl/luci-app-koolproxyR.git  package/luci-app-koolproxyR
+git clone https://github.com/tty228/luci-app-serverchan.git  package/luci-app-serverchan
+git clone https://github.com/jw8013/ddns-scripts_dnspod.git  package/ddns-scripts_dnspod
