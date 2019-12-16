e1bef2ee1b1e24755f7bc3926af9cdd3fee2e49f
jw8013 <jw8013@qq.com>
2019-12-16 15:33:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,8 +8,8 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/Ameykyl/luci-app-koolproxyR.git  package/luci-app-koolproxyR
-git clone https://github.com/tty228/luci-app-serverchan.git  package/luci-app-serverchan
-git clone https://github.com/jw8013/ddns-scripts_dnspod.git  package/ddns-scripts_dnspod
-git clone https://github.com/pymumu/smartdns.git
-git clone https://github.com/pymumu/luci-app-smartdns.git
+git clone https://github.com/Ameykyl/luci-app-koolproxyR.git package/luci-app-koolproxyR
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/jw8013/ddns-scripts_dnspod.git package/ddns-scripts_dnspod
+git clone https://github.com/pymumu/smartdns.git package/smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
